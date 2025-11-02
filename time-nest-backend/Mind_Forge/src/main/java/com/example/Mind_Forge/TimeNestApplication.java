a5c72b965ac10317002506f382720b8464e21a56
Christian <ChrisThomas0634@gmail.com>
2025-11-02 03:28:16 -0500

    Modified Docker File attempt4

--- time-nest-backend/Mind_Forge/src/main/java/com/example/Mind_Forge/TimeNestApplication.java
+++ time-nest-backend/Mind_Forge/src/main/java/com/example/Mind_Forge/TimeNestApplication.java
@@ -2,10 +2,8 @@ package com.example.Mind_Forge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:.env.properties")
 public class TimeNestApplication {
 
 	public static void main(String[] args) {

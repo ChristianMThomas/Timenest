ff3eea68922b652dd877773ce70e7e037e832642
Christian <ChrisThomas0634@gmail.com>
2025-11-02 02:49:20 -0500

    Switched to Render// Added Dockerfile

--- time-nest-backend/Mind_Forge/src/main/java/com/example/Mind_Forge/TimeNestApplication.java
+++ time-nest-backend/Mind_Forge/src/main/java/com/example/Mind_Forge/TimeNestApplication.java
@@ -2,9 +2,10 @@ package com.example.Mind_Forge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:.env.properties")
 public class TimeNestApplication {
 
 	public static void main(String[] args) {
